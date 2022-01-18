38f75249f4ce29cfe3093fa86a4c6364c4cea02d
tamizh-m <tamizhh1997@gmail.com>
2022-01-18 16:12:56 +0530

    initial commit

---
+++ src/main/java/com/tamizh/PlaceSuggester/PlaceSuggesterApplication.java
@@ -0,0 +1,13 @@
+package com.tamizh.PlaceSuggester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceSuggesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceSuggesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamizh/PlaceSuggester/PlaceSuggesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamizh.PlaceSuggester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceSuggesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
